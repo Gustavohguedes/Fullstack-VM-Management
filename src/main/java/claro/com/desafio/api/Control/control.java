29a9ea79fb4933f639679257227fa5e77897ee30
Gustavohguedes <gustavoguedesalbuquerque@gmail.com>
2025-04-10 01:35:31 -0300

    CRUD api actions

--- src/main/java/claro/com/desafio/api/Control/control.java
+++ src/main/java/claro/com/desafio/api/Control/control.java
@@ -2,10 +2,16 @@ package claro.com.desafio.api.Control;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import claro.com.desafio.api.Model.Client;
 import claro.com.desafio.api.Repository.Repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class Control {
@@ -13,9 +19,24 @@ public class Control {
     @Autowired
     private Repository acao;
 
+    @PostMapping("/")
+    public Client cadastrar(@RequestBody Client c) {
+        return acao.save(c);
+    }
+
     @GetMapping("/")
-    public String getHello() {
-        return "Hello World!";
+    public Iterable<Client> SelectAll() {
+        return acao.findAll();
+    }
+
+    @PutMapping("/")
+    public Client Update(@RequestBody Client c) {
+        return acao.save(c);
+    }
+
+    @DeleteMapping("/{codigo}")
+    public void Delete(@PathVariable long codigo) {
+        acao.deleteById(codigo);
     }
 
 }
